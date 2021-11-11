09b34e02a927e343a1ecdb4283e081661dbc51df
min050410 <min050410@gmail.com>
2021-11-11 19:41:24 +0900

    fetch를 이용해 api 가져옴

--- App.js
+++ App.js
@@ -1,31 +1,75 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { ScrollView, ActivityIndicator, Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { fetch } from 'react-native';
+import { useEffect, useState } from 'react';
 
+// function getSitename(){
+//   fetch('https://www.garak.co.kr/publicdata/dataOpen.do?id=3098&passwd=qkrandjs1%21&dataid=data4&pagesize=10&pageidx=1&portal.templet=false&p_ymd=20211111&p_jymd=20211110&d_cd=2&p_jjymd=20201111&p_pos_gubun=1&pum_nm=').then((response) => {
 
+//   return response
+//   });
+// }
+
+// 현재가를 알려주는 스크린
 function NowScreen() {
   return (
+
     <View style={styles.normal}>
       <Text>now</Text>
     </View>
-    
+
   );
 }
 
-
+//홈 화면 스크린 - 예측 예상
 function HomeScreen() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  //api 통신
+  const getPrice = async () => {
+    try {
+      const response = await fetch('https://www.garak.co.kr/publicdata/dataOpen.do?id=3098&passwd=qkrandjs1%21&dataid=data4&pagesize=10&pageidx=1&portal.templet=false&p_ymd=20211111&p_jymd=20211110&d_cd=2&p_jjymd=20201111&p_pos_gubun=1&pum_nm=');
+      const json = await response.text();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+
   return (
-    <View style={styles.normal}>
-      <Text>Home!</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        // <FlatList
+        //   data={data}
+        //   keyExtractor={({ id }, index) => id}
+        //   renderItem={({ item }) => (
+        //     <Text>{item}, {item.releaseYear}</Text>
+        //   )}
+        // />
+        
+        <ScrollView>
+          <Text>{data}</Text>
+        </ScrollView>
+      )}
     </View>
   );
 }
 
+//search 검색 스크린
 function SettingsScreen() {
   return (
     <View style={styles.normal}>
-      <Text>Settings!</Text>
+      <Text>Search!</Text>
     </View>
   );
 }
@@ -33,21 +77,24 @@ function SettingsScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="now" component={NowScreen}/>
-        <Tab.Screen style={styles.completeCircle} name="Home" component={HomeScreen}/>
+        <Tab.Screen name="now" component={NowScreen} />
+        <Tab.Screen style={styles.completeCircle} name="Home" component={HomeScreen} />
         <Tab.Screen name="Search" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   normal: {
     flex: 1,
-    justifyContent: 'center', 
+    justifyContent: 'center',
     alignItems: 'center',
   },
   completeCircle: {
