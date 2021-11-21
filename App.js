1360a17d1b40c5169f66ac84e794da6a4f2a5f12
min050410 <min050410@gmail.com>
2021-11-21 21:49:18 +0900

    add: icon 추가 앱 개발 최종 완료

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Image, ScrollView, ActivityIndicator, Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 //components
 import NowScreen from './components/NowScreen';
@@ -11,14 +11,55 @@ import SearchScreen from './components/SearchScreen';
 
 const Tab = createBottomTabNavigator();
 
+function TabBarIcon({ name, focused }) {
+  return (
+    <Ionicons
+      name={name}
+      size={25}
+      color={focused ? "#50A6FF" : "black"}
+      style={{ marginBottom: -10 }}
+    />
+  )
+}
+
 //master 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="실시간 가격" component={NowScreen} />
-        <Tab.Screen style={styles.completeCircle} name="가격 예측" component={HomeScreen} />
-        <Tab.Screen name="찾기" component={SearchScreen} />
+        <Tab.Screen 
+          name="실시간 가격" 
+          component={NowScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                focused={focused}
+                name={Platform.OS === "ios" ? `ios-help` : "ios-time-outline"}
+              />
+            ),
+          }} />
+        <Tab.Screen 
+          name="가격 예측" 
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                focused={focused}
+                name={Platform.OS === "ios" ? `ios-home` : "trending-up"}
+              />
+            ),
+          }} />
+        <Tab.Screen 
+          name="찾기" 
+          component={SearchScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                focused={focused}
+                name={Platform.OS === "ios" ? `ios-home` : "compass-outline"}
+              />
+            ),
+          }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
